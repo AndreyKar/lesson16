f93f40dc0a358f739c75ea6efba3b7297ae6941e
AndreyKar <arakka@mail.ru>
2020-02-22 12:35:42 +0300

    Lesson 14 done

--- js/script.js
+++ js/script.js
@@ -121,6 +121,47 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // Form
 
+
+        // function sendForm(formName) {
+    //     let message = {
+    //         loading: 'Загружается...',
+    //         success: 'Спасибо, скоро мы с вами свяжемся!',
+    //         failure: 'Что-то пошло не так!'
+    //     };
+
+    //     let form = document.querySelector(formName),
+    //         input = form.getElementsByTagName('input'),
+    //         statusMessage = document.createElement('div');
+
+    //     statusMessage.classList.add('status');
+
+    //     form.addEventListener('submit', function (event) {
+    //         event.preventDefault();
+    //         form.appendChild(statusMessage);
+
+    //         let request = new XMLHttpRequest();
+    //         request.open('POST', 'server.php');
+    //         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    //         let formData = new FormData(form);
+    //         request.send(formData);
+
+    //         request.addEventListener('readystatechange', function () {
+    //             if (request.readyState < 4) {
+    //                 statusMessage.innerHTML = message.loading;
+    //             } else if (request.readyState === 4 && request.status == 200) {
+    //                 statusMessage.innerHTML = message.success;
+    //             } else {
+    //                 statusMessage.innerHTML = message.failure;
+    //             }
+    //         });
+
+    //         for (let i = 0; i < input.length; i++) {
+    //             input[i].value = '';
+    //         }
+    //     });
+    // }
+
     function sendForm(formName) {
         let message = {
             loading: 'Загружается...',
@@ -137,27 +178,41 @@ window.addEventListener('DOMContentLoaded', function () {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
             form.appendChild(statusMessage);
-
-            let request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
             let formData = new FormData(form);
-            request.send(formData);
 
-            request.addEventListener('readystatechange', function () {
-                if (request.readyState < 4) {
-                    statusMessage.innerHTML = message.loading;
-                } else if (request.readyState === 4 && request.status == 200) {
-                    statusMessage.innerHTML = message.success;
-                } else {
-                    statusMessage.innerHTML = message.failure;
-                }
-            });
+            function postData(data) {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-            for (let i = 0; i < input.length; i++) {
-                input[i].value = '';
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 3) {
+                                resolve();
+                            }    
+                        } else {
+                            reject();
+                        }
+                    };
+
+                    request.send(formData);
+                });
             }
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+
+            postData(formData)
+                .then(()=> statusMessage.innerHTML = message.loading)
+                .then(()=> statusMessage.innerHTML = message.success)
+                .catch(()=> statusMessage.innerHTML = message.failure)
+                .then(clearInput);
         });
     }
 
